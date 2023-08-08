340beb675a89a2cb5de041e6b883ae38f7ba640c
juvtorg2014 <117074157+juvtorg2014@users.noreply.github.com>
2023-08-08 16:02:49 +0300

    Третий рабочий вариант

--- single.py
+++ single.py
@@ -24,21 +24,22 @@ def comparison(file_source, file_receiver):
     new_reciever['Seria'] = new_reciever['Seria'].str.strip()
     new_reciever = new_reciever.sort_values(by=['Seria'])
     
-    new_source.to_csv(file_source.replace('.xlsx', '.csv'), index=False, header=True, encoding='cp1251', sep=';')
-    new_reciever.to_csv(file_receiver.replace('.xlsx', '.csv'), index=False, header=True, encoding='cp1251', sep=';')
+    # new_source.to_csv(file_source.replace('.xlsx', '.csv'), index=False, header=True, encoding='cp1251', sep=';')
+    # new_reciever.to_csv(file_receiver.replace('.xlsx', '.csv'), index=False, header=True, encoding='cp1251', sep=';')
 
     dif_col = new_source.set_index('Seria').subtract(new_reciever.set_index('Seria'))
-    dif_table = dif_col[(dif_col['Sum'] != float(0)) | (dif_col['Commiss'] != float(0))]
+    dif_table = dif_col[(dif_col['Sum'] != float(0)) | (dif_col['Commiss'] != float(0))
+                                                     | (dif_col['Quant'] != float(0))]
 
     dif_table.to_csv('Товары.csv', index=True, header=True, encoding='cp1251', sep=';')
     print(dif_table)
 
 
 if __name__ == '__main__':
-    #file_1 = input("Введите имя файла контрагента без расширения\n")
-    file_1 = 'Кавелина-отч'
+    file_1 = input("Введите имя файла контрагента без расширения\n")
+    # file_1 = 'Кавелина-отч'
     file_1 = os.path.abspath(file_1 + '.xlsx')
-    #file_2 = input("Введите имя второго файла без расширения\n")
-    file_2 = 'Кавелина-док'
+    file_2 = input("Введите имя второго файла без расширения\n")
+    # file_2 = 'Кавелина-док'
     file_2 = os.path.abspath(file_2 + '.xlsx')
     comparison(file_1, file_2)
